1f6f64b62fd2186dce8f9539fa785be3f0b16509
Muhammadsodiq <xikmatullayevmuhammadsodiq21@gmail.com>
2026-03-23 20:12:12 +0500

    nmadirla

--- book/urls.py
+++ book/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('create_book/', views.create_book, name='create_book'),
     path('book_list/', views.book_list, name='book_list'),
     path('delete/<int:pk>/', views.delete_book, name='delete_book'),
-
+    path('edit/<int:id>/', views.edit_book, name='edit_book'),
     ]
--- book/views.py
+++ book/views.py
@@ -42,7 +42,7 @@ def create_book(request):
 
       
         image_file = fetch_book_image(title, author)
-        # image_file = fetch_book_image(author),
+        
 
         if image_file:
             book.image.save(f"{title}.jpg", image_file, save=True)
@@ -58,14 +58,28 @@ def delete_book(request, pk):
     return redirect('book_list')
 
 
-def fetch_book_image(title):
-    url = f"https://www.googleapis.com/books/v1/volumes?q={title}"
+def fetch_book_image(title, author):
+    query = f"{title}+inauthor:{author}"
+    url = f"https://www.googleapis.com/books/v1/volumes?q={query}"
+
     response = requests.get(url)
     data = response.json()
 
     try:
-        img_url = data['items'][0]['volumeInfo']['imageLinks']['thumbnail']
-        img_response = requests.get(img_url)
-        return ContentFile(img_response.content)
+        image_url = data["items"][0]["volumeInfo"]["imageLinks"]["thumbnail"]
+        image_response = requests.get(image_url)
+        return ContentFile(image_response.content)
     except:
         return None
+
+
+def edit_book(request, id):
+    book = get_object_or_404(Book, id=id)
+
+    if request.method == "POST":
+        book.title = request.POST.get("title")
+        book.author = request.POST.get("author")
+        book.save()
+        return redirect("book_list")
+
+    return render(request, "create_book.html", {"book": book})
